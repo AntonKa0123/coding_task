1c336b364f9ecd061fbe56b29bf44b9837a0f8d7
anton <irbis-1989@mail.ru>
2021-05-05 16:04:37 +0300

    removed unused imports

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -5,7 +5,6 @@ import models.Post;
 import models.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import properties.PropertiesLoader;
 
 import java.util.ArrayList;
 import java.util.HashMap;
